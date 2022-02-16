3935065f07df6607a0282b16c97fb5c30ff2bf0a
YitavGil <yitavga@yahoo.com>
2022-02-16 19:50:49 +0200

    update todos

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import Form from './comps/Form';
 import TodoList from './comps/TodoList';
 
 function App() {
-  const [inputText, setInputText] = useState('')
+  const [inputText, setInputText] = useState('');
+  const [todos, setTodos] = useState([])
+
   return (
     <div className="App">
       <header>
       <h1>Todo List</h1>
       </header>
-      <Form />
-      <TodoList />
+      <Form 
+      todos={todos} 
+      setTodos={setTodos} 
+      setInputText={setInputText}
+      inputText={inputText}
+      />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
